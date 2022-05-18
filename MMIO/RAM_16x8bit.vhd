43ab7f9864eca85d0be8ef68a982839c8dc37c10
lecsaaa <100294875+lecsaaa@users.noreply.github.com>
2022-05-18 17:01:23 +0200

    Update RAM_16x8bit.vhd

--- MMIO/RAM_16x8bit.vhd
+++ MMIO/RAM_16x8bit.vhd
@@ -4,22 +4,19 @@ use ieee.numeric_std.all;
 
 entity sprf_async_out is
 	port (
-		clk:		in	std_logic;
+		clk:			in	std_logic;
 		we:			in	std_logic;
 		addr_write:		in	std_logic_vector (15 downto 0);
 		addr_read:		in	std_logic_vector (15 downto 0);
-		data_in:	in	std_logic_vector (7 downto 0);
-		data_out:	out std_logic_vector (7 downto 0)
+		data_in:		in	std_logic_vector (7 downto 0);
+		data_out:		out	std_logic_vector (7 downto 0)
 );
 end entity sprf_async_out;
 
 architecture rtl of sprf_async_out is
 
 	type content_t is array (0 to 15) of std_logic_vector (7 downto 0);
-	signal content: content_t := (
-		0 => X"1111",
-		1 => X"0000"
-	);
+	signal content: content_t;
 
 begin
 
